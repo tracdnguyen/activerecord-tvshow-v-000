9f76dffcb7507e2914478e44f8b052186d9e971a
Trac Nguyen <nguyendtrac@gmail.com>
2018-06-10 23:39:00 +0000

    automatically backed up by learn

--- db/migrate/001_create_shows.rb
+++ db/migrate/001_create_shows.rb
@@ -1,8 +1,10 @@
 class CreateShows < ActiveRecord::Migration
-  create_table :shows do |t|
-    t.string :name
-    t.string :network
-    t.string :day
-    t.integer :rating
-
+  def change
+    create_table :shows do |t|
+      t.string :name
+      t.string :network
+      t.string :day
+      t.integer :rating
+    end
+  end
 end
